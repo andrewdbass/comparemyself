5edb8c5d623b43adfb18ae9ae564597642c217c5
andrewdbass <andybass12@gmail.com>
2016-12-14 14:15:58 -0700

    fleshed out top component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { TopComponent } from './top/top.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopComponent
   ],
   imports: [
     BrowserModule,
--- src/app/top/top.component.ts
+++ src/app/top/top.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-top',
@@ -6,9 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./top.component.css']
 })
 export class TopComponent implements OnInit {
-
+  @Output() age = new EventEmitter();
+  private ageInput:number;
   constructor() { }
-
+  private newAge(){
+    if(this.ageInput>0){
+      console.log(this.ageInput)
+      this.age.emit(this.ageInput);
+    }
+  }
   ngOnInit() {
   }
 

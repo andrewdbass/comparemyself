519cbdea87451280fb231a138c1f5017392f4e72
andrewdbass <andybass12@gmail.com>
2016-12-14 18:03:37 -0700

    started fleshing out the list and added firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { AngularFireModule } from 'angularfire2';
 
 import { AppComponent } from './app.component';
 import { TopComponent } from './top/top.component';
+import { ListComponent } from './list/list.component';
+
+export const firebaseConfig = {
+  apiKey: 'AIzaSyAIZ4JWGeG5SBvydPQZ-XkP2BCWLmG5usw',
+  authDomain: 'comparemyself-ef3d1.firebaseapp.com',
+  databaseURL: 'https://comparemyself-ef3d1.firebaseio.com',
+  storageBucket: 'comparemyself-ef3d1.appspot.com'
+};
 
 @NgModule({
   declarations: [
     AppComponent,
-    TopComponent
+    TopComponent,
+    ListComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFire, FirebaseListObservable } from 'angularfire2';
 
 @Component({
   selector: 'app-list',
@@ -6,10 +7,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list.component.css']
 })
 export class ListComponent implements OnInit {
-
-  constructor() { }
+  private people: FirebaseListObservable<any[]>;
+  constructor(
+    af: AngularFire
+  ) {
+    this.people = af.database.list('/people');
+  }
 
   ngOnInit() {
+    // this.people.push(
+    //   {
+    //     name:"Elon Musk",
+    //     image: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/49/Elon_Musk_2015.jpg/220px-Elon_Musk_2015.jpg"
+    //   }
+    // );
+    // this.people.push(
+    //   {
+    //     name:"Steve Jobs",
+    //     image: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b9/Steve_Jobs_Headshot_2010-CROP.jpg/220px-Steve_Jobs_Headshot_2010-CROP.jpg"
+    //   }
+    // );
+    // this.people.push(
+    //   {
+    //     name:"Mark Zuckerberg",
+    //     image: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fe/Mark_Zuckerberg_em_setembro_de_2014.jpg/220px-Mark_Zuckerberg_em_setembro_de_2014.jpg"
+    //   }
+    // );
+    console.log(this.people);
   }
 
 }

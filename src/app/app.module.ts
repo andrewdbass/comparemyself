16a37f1a6ef8c880513f4c3f0297c180d8362aad
andrewdbass <andybass12@gmail.com>
2016-12-16 08:47:26 -0700

    style grid lsit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,30 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { AngularFireModule } from 'angularfire2';
+import {MdGridListModule} from '@angular2-material/grid-list/grid-list.js';
 
 import { AppComponent } from './app.component';
 import { TopComponent } from './top/top.component';
 import { ListComponent } from './list/list.component';
-
-export const firebaseConfig = {
-  apiKey: 'AIzaSyAIZ4JWGeG5SBvydPQZ-XkP2BCWLmG5usw',
-  authDomain: 'comparemyself-ef3d1.firebaseapp.com',
-  databaseURL: 'https://comparemyself-ef3d1.firebaseio.com',
-  storageBucket: 'comparemyself-ef3d1.appspot.com'
-};
+import { PersonComponent } from './person/person.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TopComponent,
-    ListComponent
+    ListComponent,
+    PersonComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
-    AngularFireModule.initializeApp(firebaseConfig)
+    MdGridListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
